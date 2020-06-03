7ced96dcc90a7ca83af70c1ac86c5b2548693cff
Ian Felix <ianfelix030@gmail.com>
2020-06-03 12:16:00 -0300

    Update script.js

--- modulo02/trabalho-pratico-2/script.js
+++ modulo02/trabalho-pratico-2/script.js
@@ -1,9 +1,45 @@
-import fs from 'fs'
+import fs from 'fs';
 
-const cityFile = 'modulo02/trabalho-pratico-2/Cidades.json'
-const stateFile = 'modulo02/trabalho-pratico-2/Estados.json'
+const cityFile = 'modulo02/trabalho-pratico-2/Cidades.json';
+const stateFile = 'modulo02/trabalho-pratico-2/Estados.json';
 
-function writeFile() {
-  fs.writeFile(stateFile, '' , )
+function readState() {
+  fs.readFile(stateFile, function dataStatef(err, data) {
+    const dataState = JSON.parse(data);
+    creatFile(dataState);
+    console.log(dataState);
+    return dataState;
+  });
 }
 
+function readCity() {
+  fs.readFile(cityFile, function dataCityf(err, data) {
+    const dataCity = JSON.parse(data);
+
+    return dataCity;
+  });
+}
+
+async function creatFile(arrayState) {
+  for (let i = 0; i < arrayState.length; i++) {
+    fs.writeFile(
+      `modulo02/trabalho-pratico-2/city/${arrayState[i].Sigla}.json`,
+      '',
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+}
+
+function stateAndCity(cityNumber, id) {
+  for (let i = 0; i < cityNumber.length; i++) {
+    if (cityNumber[i].Estado === id[i].ID) {
+      
+    }
+  }
+}
+
+readCity();
+readState();
+stateAndCity(readCity(), readState());
